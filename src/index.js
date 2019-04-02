0942d233952feeb247099e911cc992beb9314c1f
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-04-02 18:15:21 +0200

    adds useRange hook

--- src/index.js
+++ src/index.js
@@ -9,8 +9,29 @@ function useCounter(step = 1, defaultValue = 0) {
   return [count, inc, dec, setCount];
 }
 
+function useRange({ min = 0, max = 100, step = 1, defaultValue = 50 } = {}) {
+  const [count, setCount] = useState(defaultValue);
+
+  const toFixed = expr => {
+    return parseFloat(expr.toFixed(2));
+  };
+
+  const decrement = () =>
+    setCount(c => (toFixed(c - step) >= min ? toFixed(c - step) : c));
+  const increment = () =>
+    setCount(c => (toFixed(c + step) <= max ? toFixed(c + step) : c));
+
+  return [count, increment, decrement, setCount];
+}
+
 function App() {
-  const [zoomValue, inc, dec, setZoomValue] = useCounter(0.1, 1);
+  const rangeOptions = {
+    min: 0,
+    max: 2,
+    step: 0.1,
+    defaultValue: 1
+  };
+  const [zoomValue, inc, dec, setZoomValue] = useRange(rangeOptions);
 
   function handleZoomChange(e) {
     setZoomValue(parseFloat(e.target.value));
@@ -26,14 +47,14 @@ function App() {
       <div style={{ zoom: zoomValue, backgroundColor: "#EAEAEA" }}>
         <h1>Hello CodeSandbox</h1>
       </div>
-      <div className="zoom">
+      <div className="range-control">
         <button onClick={handleZoomDec}>-</button>
         <input
           type="range"
-          min="0.3"
-          max="5"
+          min={rangeOptions.min}
+          max={rangeOptions.max}
           value={zoomValue}
-          step=".1"
+          step={rangeOptions.step}
           onChange={handleZoomChange}
         />
         <button onClick={handleZoomInc}>+</button>
